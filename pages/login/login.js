143afee63ea44f4fa92362163bee2becf91230f1
Jinjin123 <1293813551@qq.com>
2019-02-11 15:40:17 +0800

    update

--- pages/login/login.js
+++ pages/login/login.js
@@ -17,30 +17,33 @@ data:{},
 
   bindFormSubmit(event){
     var data=event.detail.value;
-    wx.request({
-      url: 'http://localhost:3000/login',
-      method:'POST',
-      data:data,
-      header:{
-        'content-type':'application/x-www-form-urlencoded'
-      },
-      success:function(res){
-        if (res.data.code == 0){
-          wx.setStorageSync('token',res.data.token)
+             wx.switchTab({
+            url: "/pages/indexguanzhu/indexguanzhu",
+          })
+    // wx.request({
+    //   url: 'http://localhost:4000/login',
+    //   method:'POST',
+    //   data:data,
+    //   header:{
+    //     'content-type':'application/x-www-form-urlencoded'
+    //   },
+    //   success:function(res){
+    //     if (res.data.code == 0){
+    //       wx.setStorageSync('token',res.data.token)
 
-          wx.switchTab({
-            url: "/pages/indexguanzhu/indexguanzhu",
-          })
-          wx.navigateTo({
-            url: "/pages/indexguanzhu/indexguanzhu",
-          })
-        }else{
-          wx.showToast({
-            title: '失败，请检查你的账号密码',
-            icon:'none'
-          })
-        }
-      }
-    })
+    //       wx.switchTab({
+    //         url: "/pages/indexguanzhu/indexguanzhu",
+    //       })
+    //       wx.navigateTo({
+    //         url: "/pages/indexguanzhu/indexguanzhu",
+    //       })
+    //     }else{
+    //       wx.showToast({
+    //         title: '失败，请检查你的账号密码',
+    //         icon:'none'
+    //       })
+    //     }
+    //   }
+    // })
   }
 })
